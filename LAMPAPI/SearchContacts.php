ae70ae4e239d2a0640a57b630ae310985306bb0c
magnvsus <magnvsus@gmail.com>
2026-05-23 15:29:27 -0400

    feat: added contact search behavior

---
+++ LAMPAPI/SearchContacts.php
@@ -0,0 +1,71 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+
+		# Search for like terms in all field—first and last name, email, and phone.
+		$stmt = $conn->prepare("select Name from Contacts where FirstName like :searchQuery or LastName like :searchQuery or Phone like :searchQuery or Email like :searchQuery AND UserID = :userID" );
+		$searchQuery = "%" . $inData["search"] . "%";
+		$userID = $indata["userID"];
+		#$stmt->bind_param("ss", $searchQuery);
+		$stmt->execute([':searchQuery'=>$searchQuery, ':userID'=$userID]);
+		
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["Name"] . '"';
+		}
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
